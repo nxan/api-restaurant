6884c2e57918dc937374a4c9deb7970927809add
nxan <nxan.developer@gmail.com>
2019-06-14 10:36:58 +0700

    commit

--- api-restaurant/app/db-config.js
+++ api-restaurant/app/db-config.js
@@ -4,14 +4,14 @@ const mssql = require('mssql');
 const db = new mssql.ConnectionPool({
     user: "nxan",
     password: "Nguyenxuanan1811",
-    server: "nxan.database.windows.net",
-    database: "Restaurant",
+    server: "nxan-azure.database.windows.net",
+    database: "NhaHang",
     port: 1433,
-    encrypt: true 
+    encrypt: true
 });
 
 db.connect((err) => {
-    if(err) {
+    if (err) {
         throw err;
     }
     console.log("Mssql connected...")
